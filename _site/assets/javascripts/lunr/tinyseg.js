663d00aff6a3c7e6506a92affc940a371bbadaf0
Sapta Dey <142305749+SaptaDey@users.noreply.github.com>
2025-05-29 23:14:30 +0200

    Update _site/assets/javascripts/lunr/tinyseg.js
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- _site/assets/javascripts/lunr/tinyseg.js
+++ _site/assets/javascripts/lunr/tinyseg.js
@@ -116,7 +116,7 @@
           var seg = ["B3","B2","B1"];
           var ctype = ["O","O","O"];
           var o = input.split("");
-          for (i = 0; i < o.length; ++i) {
+          for (let i = 0; i < o.length; ++i) {
             seg.push(o[i]);
             ctype.push(this.ctype_(o[i]))
           }
